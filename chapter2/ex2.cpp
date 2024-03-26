501d647d196191e37474c8e2cc8f5ee06b3fc156
TonyQ <150294902+tonyqua@users.noreply.github.com>
2024-03-26 02:47:19 +0200

    Add files via upload

---
+++ chapter2/ex2.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Anton on 26.03.2024.
+//
+#include <stdio.h>
+#define ROW 5
+
+int main() {
+    int row;
+    int column;
+
+    for(row = 0; row < ROW; row++){
+        for(column = 0; column <= row; column++){
+            printf("$");
+        }
+        printf("\n");
+    }
+    return 0;
+}
